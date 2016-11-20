98b9ec0bd0d0ccff20bc6949a78c5922e5441521
Wilson Cai <wcai@berkeley.edu>
2016-11-19 23:01:21 -0800

    eg 1

--- code/example1.py
+++ code/example1.py
@@ -19,7 +19,6 @@ import simulation
 
 # EG1: f(x) = -x**4
 degree = 4
-maxIter = int(1e5)
 def func(x, degree = degree):
     return -np.power(x, degree)
 def grad(x, degree = degree):
@@ -34,12 +33,15 @@ modes = [0]
 # ha = trajectory[haltIter/2:haltIter]
 
 # all_simu = simulation.simu_all_parallel(n_sim = 1e1, func = func, grad = grad, initialPoint=1., stepsize=1e-2/2, noiseLevel=1e-1, maxIter=int(1e5), desiredObj=100)
-all_traject = simu_all_parallel(n_sim = 1e2, func = func, grad = grad, initialPoint=1., stepsize=1e-2/2, noiseLevel=1e-1, maxIter=int(1e5), desiredObj=100)
+all_traject = simu_all_parallel(n_sim = 1e2, func = func, grad = grad,
+                                initialPoint=1., stepsize=1e-2/2, noiseLevel=1e-1,
+                                 maxIter=int(1e5), desiredObj=100)
+# 100 n_sim = 1.25 min
 
-mix_time = mixing.mixing_time(all_trajectory = all_traject, station_func = station_func, epsilon_norm=1e-1, a=-3, b=3, dx=.01)
-
-conv_time_all = convergence.convergence_time_all(all_trajectory = all_traject, modes = modes, epsilon_neighbor=1e-2)
-
+mix_time = mixing.mixing_time(all_trajectory = all_traject, station_func = station_func,
+                                 epsilon_norm=1e-1, a=-3, b=3, dx=.01)
+conv_time_all = convergence.convergence_time_all(all_trajectory = all_traject,
+                                                 modes = modes, epsilon_neighbor=1e-2)
 
 np.median(conv_time_all)
 mix_time
